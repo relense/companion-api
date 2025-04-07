e96b9dd76d171a80b25e3f3de5591386ae45077d
Miguel <miguelmofurtado@gmail.com>
2025-04-07 17:01:24 +0100

    update

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,15 @@ const startServer = () =>
   setupExpressApp({
     express: {
       port: parseInt(process.env.PORT || "4000"),
+      preInit: (app) => {
+        if (process.env.NODE_ENV === "development") {
+          mountSwaggerUi({
+            filePath: "./api/openapi-users.json",
+            mountPoint: "/api-docs-users",
+            app,
+          });
+        }
+      },
       postInit: (app) => {
         app.use("/admin", adminAuth, adminRouter);
         app.use("/client", clientAuth, clientRouter);
