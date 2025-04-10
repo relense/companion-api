a7a83825d2c9451a13866130fdc0a2830395c46d
Miguel <miguelmofurtado@gmail.com>
2025-04-10 16:48:13 +0100

    clean code

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,12 +1,15 @@
-import { prisma } from "../prisma.js";
 import { Errors } from "../utils/errors.js";
 
 async function validateUser(params: { userToken: string }) {
-  const user = await prisma.user.findFirst({
-    where: {
-      token: params.userToken,
-    },
-  });
+  const user = {
+    id: "djiosadioasjdoiasjo",
+    role: "CLIENT",
+  };
+  // const user = await prisma.user.findFirst({
+  //   where: {
+  //     token: params.userToken,
+  //   },
+  // });
 
   if (!user) {
     throw Errors.userNotFound(params.userToken);
